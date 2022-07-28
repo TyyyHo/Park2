525a6a883957ea64e033c3a1e34ced4a5237aa57
TyyyHo <ty.ho1126@gmail.com>
2022-07-28 11:06:39 +0800

    reset

--- src/api/url.js
+++
@@ -1,6 +0,0 @@
-//教室
-// export const url = "http://10.0.102.245:3001"
-// 其他
-export var url = "http://192.168.100.11:3001"
-
-
--- src/components/pages/map/component/map_mobile_bg.jsx
+++
@@ -1,9 +0,0 @@
-const Mobile_bg = () => {
-  return (
-    <div id="mobile_bg">
-      <p>點擊空白處返回</p>
-    </div>
-  );
-};
-
-export default Mobile_bg;
