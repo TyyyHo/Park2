7ab11f8a3893d380b1dd096d62a8bfffe2a29f07
TyyyHo <ty.ho1126@gmail.com>
2022-07-28 16:15:27 +0800

    0

--- src/components/pages/map/component/map_sideBar.jsx
+++ src/components/pages/map/component/map_sideBar.jsx
@@ -9,24 +9,24 @@ class SideBar extends Component {
         <div id="secondBar">
           <span id="ctn1">
             <div id="toilet_icon">
-              <img src="/map/icon/wc.svg" alt="wc" />
+              <img src="map/icon/wc.svg" alt="wc" />
               <p>洗手間</p>
             </div>
             <br />
             <div id="parking_icon">
-              <img src="/map/icon/parking.svg" alt="" />
+              <img src="map/icon/parking.svg" alt="" />
               <p>停車場</p>
             </div>
           </span>
 
           <span id="ctn2">
             <div id="basketball_icon">
-              <img src="/map/icon/basketball.svg" alt="" />
+              <img src="map/icon/basketball.svg" alt="" />
               <p> 球場 </p>
             </div>
             <br />
             <div id="elevator_icon">
-              <img src="/map/icon/elevator.svg" alt="" />
+              <img src="map/icon/elevator.svg" alt="" />
               <p> 電梯 </p>
             </div>
           </span>
